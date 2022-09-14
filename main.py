2ef50363826ddfc53c020f8f2ec2f18a0233945e
ronnieketashvili <ronnie.k5k@gmail.com>
2022-09-14 19:42:29 +0300

    nothing

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import MineField
 import consts
 import screen
 import pygame
-
 import soldier
 
 
-def draw_screen():
-    SCREEN_COLOR = (61, 145, 64)
-    screen.SCREEN.fill(SCREEN_COLOR)
-    screen.SCREEN.blit(soldier.soldier_image, (soldier.soldier.x, soldier.soldier.y))
-    pygame.display.update()
+
 
 
 def maim(): #פה ירוץ המשחק
@@ -23,7 +18,7 @@ def maim(): #פה ירוץ המשחק
             if event.type == pygame.QUIT:
                 finish = True
         soldier.soldier_moving()
-        draw_screen()
+        screen.opening_screen()
     pygame.quit()
 if __name__ == '__main__':
     maim()
--- screen.py
+++ screen.py
@@ -1,23 +1,35 @@
 import pygame
 import consts
+import random
 import os
-
+import soldier
 pygame.init()
-size = (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT)
-SCREEN = pygame.display.set_mode(size)
+SCREEN = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 pygame.display.set_caption("FLAG GAME!")
 
-bush_picture = pygame.image.load(os.path.join('bush.png'))
-bush = pygame.transform.scale(bush_picture, (400,200))
-SCREEN.blit(bush, (100, 100))
+def bushes_locations():
+    locations_list = []
+    for i in range(consts.BUSHES_NUMBER):
+        x_rate = random.randint(0, 780)
+        y_rate = random.randint(consts.SOLDIER_HEIGHT, 440)
+        locations_list.append((x_rate, y_rate))
+    return locations_list
+
+locations = []
+locations = bushes_locations()
+
+def create_bushes():
+    global locations
+    for i in range(len(locations)):
+        bush_picture = pygame.image.load('bush.png')
+        bush = pygame.transform.scale(bush_picture, (consts.BUSHES_SIZE))
+        SCREEN.blit(bush, (locations[i][0], locations[i][1]))
+def opening_screen():
+    SCREEN.fill(consts.SCREEN_COLOR)
+    create_bushes()
+    SCREEN.blit(soldier.soldier_image, (soldier.soldier.x, soldier.soldier.y))
+    pygame.display.update()
+
+def when_enter_pressed():
 
 
-
-
-'''     
-img = pygame.image.load('bushes.png')
-PURPLE = (103,17,136)
-img.set_colorkey(PURPLE)
-screen.blit(img, (-100,-200))
-pygame.display.flip()
-'''
