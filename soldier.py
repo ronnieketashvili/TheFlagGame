756ca75d4b7b1fbfe2193265a33ffdf9ad7c72a4
yarin_na <yarin.naor1234@gmail.com>
2022-09-15 12:06:08 +0300

    !

--- soldier.py
+++ soldier.py
@@ -8,13 +8,13 @@ soldier_image = pygame.transform.scale(soldier_image, (consts.SOLDIER_WIDTH,cons
 
 def soldier_moving():
     keys_pressed = pygame.key.get_pressed()
-    if keys_pressed[pygame.K_RIGHT] and soldier.x + consts.VEL + consts.SOLDIER_WIDTH < BORDER.x:
+    if keys_pressed[pygame.K_RIGHT] and soldier.x + consts.VEL + consts.SOLDIER_WIDTH < consts.WINDOW_WIDTH:
         soldier.x += consts.VEL
-    elif keys_pressed[pygame.K_UP] and soldier.y - consts.VEL > 0:
+    if keys_pressed[pygame.K_UP] and soldier.y - consts.VEL > 0:
         soldier.y -= consts.VEL
-    elif keys_pressed[pygame.K_LEFT] and soldier.x - consts.VEL > 0:
+    if keys_pressed[pygame.K_LEFT] and soldier.x - consts.VEL > 0:
         soldier.x -= consts.VEL
-    elif keys_pressed[pygame.K_DOWN] and soldier.y + consts.VEL + consts.SOLDIER_HEIGHT < BORDER.y:
+    if keys_pressed[pygame.K_DOWN] and soldier.y + consts.VEL + consts.SOLDIER_HEIGHT < BORDER.y:
         soldier.y += consts.VEL
 
 BORDER = pygame.Rect(1000,500,consts.WINDOW_WIDTH,consts.WINDOW_HEIGHT)
@@ -29,17 +29,18 @@ def soldier_in_matrix(minefield):
 soldier = pygame.Rect(0,0,consts.SOLDIER_WIDTH,consts.SOLDIER_HEIGHT)
 
 def checking_minefield():
+    soldier_moving()
     if soldier_in_matrix(consts.MINEFIELD) == 'mine':
         #soldier_image = pygame.image.load('injury_soldier.png')
         #soldier_image = pygame.transform.scale(soldier_image, (consts.SOLDIER_WIDTH,consts.SOLDIER_HEIGHT))
         #screen.draw_lose_message()
         consts.FINISH = True
+        return consts.FINISH
     if soldier_in_matrix(consts.MINEFIELD) == 'flag':
         #screen.draw_win_message()
         consts.FINISH = True
+        return consts.FINISH
     else:
         soldier_moving()
 
-checking_minefield()
-print(soldier.x, soldier.y)
 
