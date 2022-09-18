426ad4a04dc593b8c88e62632a2dab50b21d29d3
Ronnie Ketashvili <ronnie.k5k@gmail.com>
2022-09-18 18:10:48 +0300

    changed

--- Database.py
+++ Database.py
@@ -15,12 +15,14 @@ df = {
     8: ["list of bushes", "list of mines", "X rate of soldier", "Y rate of soldier"],
     9: ["list of bushes", "list of mines", "X rate of soldier", "Y rate of soldier"]
 }
-def getting_data_for_press(press_number):
-    df[press_number][0] = screen.locations
-    df[press_number][1] = screen.LIST
-    df[press_number][2] = soldier.soldier.x
-    df[press_number][3] = soldier.soldier.y
+def getting_data_for_press(pressed_number):
+    df[pressed_number][0] = screen.locations
+    df[pressed_number][1] = screen.LIST
+    df[pressed_number][2] = soldier.soldier.x
+    df[pressed_number][3] = soldier.soldier.y
     df.to_csv("MemoryTheFlagGame.csv")
+
+
 
 
 df = pd.DataFrame(df)
